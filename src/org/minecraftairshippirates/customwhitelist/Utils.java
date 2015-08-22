829415d4f6bfd78f92316961e00a0eb947e1a6bd
WhiteWolfdoge <emjwhite@outlook.com>
2015-08-21 17:05:55 -0700

    Created Utils class for housing utility methods that are publicly accessible

---
+++ src/org/minecraftairshippirates/customwhitelist/Utils.java
@@ -0,0 +1,9 @@
+package org.minecraftairshippirates.customwhitelist;
+
+/**
+ * This class is for housing utility methods that are publicly accessible
+ *
+ */
+public class Utils{
+	// TODO Write page downloader
+}
