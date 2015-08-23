d0783e80da5bb56b4a205cb81ce33559c6ae93c8
WhiteWolfdoge <emjwhite@outlook.com>
2015-08-22 18:05:13 -0700

    Added check feature
    Verified working properly

--- src/org/minecraftairshippirates/customwhitelist/CustomWhitelistPlugin.java
+++ src/org/minecraftairshippirates/customwhitelist/CustomWhitelistPlugin.java
@@ -12,7 +12,8 @@ public final class CustomWhitelistPlugin extends JavaPlugin{
 	private final String MSG_TOO_MANY_ARGS = "Too many arguments!",
 		MSG_INSUFFICIENT_PERMS = "You don't have permission to do that!",
 		MSG_ADD_USAGE = "Usage: /customwhitelist add <player>",
-		MSG_REMOVE_USAGE = "Usage: /customwhitelist remove <player>";
+		MSG_REMOVE_USAGE = "Usage: /customwhitelist remove <player>",
+		MSG_CHECK_USAGE = "Usage: /customwhitelist check <player>";
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
@@ -81,6 +82,38 @@ public final class CustomWhitelistPlugin extends JavaPlugin{
 					}
 					else{ // No player was typed
 						sender.sendMessage(ChatColor.RED + MSG_REMOVE_USAGE);
+						return true;
+					}
+				}
+				else if(args[0].equalsIgnoreCase("check")){ // If the subcommand was "check"
+					if(!sender.hasPermission("customWhitelist.check")){ // If the sender doesn't have the permission for this command
+						sender.sendMessage(ChatColor.RED + MSG_INSUFFICIENT_PERMS);
+						return true;
+					}
+					else if(args.length == 2){ // If there is a player
+						try{
+							UUID uuid = UUID.fromString(UUIDFetcher.getUUID(args[1]));
+							OfflinePlayer ofp = getServer().getOfflinePlayer(uuid);
+							if(ofp.isWhitelisted()){ // If the player is whitelisted
+								sender.sendMessage('\"' + args[1] + "\" is on the whitelist.");
+							}
+							else{ // Else the player is not whitelisted
+								sender.sendMessage('\"' + args[1] + "\" is not on the whitelist.");
+							}
+						}
+						catch(UUIDNotFoundException e){
+							sender.sendMessage(ChatColor.RED.toString() + '\"' + args[1] + "\" was not found and could not be checked.");
+						}
+						
+						return true;
+					}
+					else if(args.length >= 3){ // Too many arguments
+						sender.sendMessage(ChatColor.RED + MSG_TOO_MANY_ARGS);
+						sender.sendMessage(ChatColor.RED + MSG_CHECK_USAGE);
+						return true;
+					}
+					else{ // No player was typed
+						sender.sendMessage(ChatColor.RED + MSG_CHECK_USAGE);
 						return true;
 					}
 				}
