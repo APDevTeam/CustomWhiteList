a801f3027709565f0594451ca9b7ed7bfdc14622
WhiteWolfdoge <christopherhunt7@live.com>
2015-08-31 14:28:39 -0700

    Created class CWCU for creating new queueable tasks

---
+++ src/org/minecraftairshippirates/customwhitelist/CWExuctionUnit.java
@@ -0,0 +1,8 @@
+package org.minecraftairshippirates.customwhitelist;
+
+/**
+ * This class is to house all necessary components for executing a CW function
+ */
+public class CWExuctionUnit{
+	// TODO Everything
+}
