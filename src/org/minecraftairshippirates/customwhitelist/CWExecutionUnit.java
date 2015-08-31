98263240a268f8f1d275b12d9c3b92d0f3f046d4
WhiteWolfdoge <christopherhunt7@live.com>
2015-08-31 14:48:42 -0700

    Added type validation

--- src/org/minecraftairshippirates/customwhitelist/CWExecutionUnit.java
+++ src/org/minecraftairshippirates/customwhitelist/CWExecutionUnit.java
@@ -9,14 +9,27 @@ public class CWExecutionUnit{
 	private static final int TYPE_LIST_WITH_RESOLVE = 0,
 		TYPE_ADD_USER_BY_NAME = 1,
 		TYPE_REMOVE_USER_BY_NAME = 2;
+	private static final int[] VALID_TYPES = {0, 1, 2};
 	private final int type;
 	private final CommandSender sender;
 	private final String[] subCmdArgs, subCmdOpts;
 	
-	CWExecutionUnit(int newType, CommandSender newSender, String[] newSubCmdArgs, String[] newSubCmdOpts){
+	CWExecutionUnit(int newType, CommandSender newSender, String[] newSubCmdArgs, String[] newSubCmdOpts) throws InvalidCWCUTypeException{
+		boolean typeIsValid = false;
+		for(int i : VALID_TYPES){ // For every element in VALID_TYPES
+			if(i == newType){ // If the current element matches newType
+				typeIsValid = true; // Mark it as valid
+				break; // Then exit the loop
+			}
+		}
+		if(!typeIsValid){ // If the type is not valid
+			throw new InvalidCWCUTypeException();
+		}
 		type = newType;
 		sender = newSender;
 		subCmdArgs = newSubCmdArgs;
 		subCmdOpts = newSubCmdOpts;
 	}
+	
+	// TODO Add processing methods
 }
