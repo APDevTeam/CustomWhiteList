acb298dc300b838d5936f1fe4801762a3c7e0bd1
WhiteWolfdoge <emjwhite@outlook.com>
2015-08-31 21:48:08 -0700

    Added exception for when a username cannot be found by uuid

---
+++ src/org/minecraftairshippirates/customwhitelist/UsernameNotFoundException.java
@@ -0,0 +1,26 @@
+package org.minecraftairshippirates.customwhitelist;
+
+@SuppressWarnings("serial")
+public class UsernameNotFoundException extends Exception{
+
+	public UsernameNotFoundException(){
+		// Do nothing
+	}
+
+	public UsernameNotFoundException(String arg0){
+		super(arg0);
+	}
+
+	public UsernameNotFoundException(Throwable arg0){
+		super(arg0);
+	}
+
+	public UsernameNotFoundException(String arg0, Throwable arg1){
+		super(arg0, arg1);
+	}
+
+	public UsernameNotFoundException(String arg0, Throwable arg1, boolean arg2, boolean arg3){
+		super(arg0, arg1, arg2, arg3);
+	}
+
+}
