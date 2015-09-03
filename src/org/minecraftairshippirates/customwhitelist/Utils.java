935f08caa22b6766264ae71a41af0cce43a924f3
WhiteWolfdoge <emjwhite@outlook.com>
2015-09-02 22:40:57 -0700

    Rethrew SocketTimeoutException as IOexception

--- src/org/minecraftairshippirates/customwhitelist/Utils.java
+++ src/org/minecraftairshippirates/customwhitelist/Utils.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
 import java.net.URL;
 
 /**
@@ -19,34 +20,39 @@ public class Utils{
 	 * @throws	IOException			When something goes terribly wrong
 	 */
 	public static String downloadPage(URL source) throws IOException{
-		HttpURLConnection conn = (HttpURLConnection)source.openConnection();
-		// Set timeouts
-		conn.setConnectTimeout(1000);
-		conn.setReadTimeout(2500);
+		try{
+			HttpURLConnection conn = (HttpURLConnection)source.openConnection();
+			// Set timeouts
+			conn.setConnectTimeout(1000);
+			conn.setReadTimeout(2500);
+			
+			// I'll pretend that I'm wget from cygwin!
+			conn.setRequestProperty("User-Agent", "Wget/1.16.2 (cygwin)");
+			conn.setRequestProperty("Accept", "*/*");
+			conn.setRequestProperty("Accept-Encoding", "identity");
+			conn.setRequestProperty("Host", "mcuuid.net");
+			conn.setRequestProperty("Connection", "Keep-Alive");
 		
-		// I'll pretend that I'm wget from cygwin!
-		conn.setRequestProperty("User-Agent", "Wget/1.16.2 (cygwin)");
-		conn.setRequestProperty("Accept", "*/*");
-		conn.setRequestProperty("Accept-Encoding", "identity");
-		conn.setRequestProperty("Host", "mcuuid.net");
-		conn.setRequestProperty("Connection", "Keep-Alive");
-	
-		conn.setRequestMethod("GET");
-		
-		if(!conn.getResponseMessage().contains("OK")){ // If it wasn't okay
-			throw new IOException(); // Then we give up
+			conn.setRequestMethod("GET");
+			
+			if(!conn.getResponseMessage().contains("OK")){ // If it wasn't okay
+				throw new IOException(); // Then we give up
+			}
+			
+			StringBuilder sb = new StringBuilder();
+			
+			InputStream is = conn.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			
+			String resp;
+			while((resp = br.readLine()) != null){ // While what we are reading is data
+				sb.append(resp); // Add the data
+			}
+			
+			return sb.toString();
 		}
-		
-		StringBuilder sb = new StringBuilder();
-		
-		InputStream is = conn.getInputStream();
-		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-		
-		String resp;
-		while((resp = br.readLine()) != null){ // While what we are reading is data
-			sb.append(resp); // Add the data
+		catch(SocketTimeoutException stex){
+			throw new IOException();
 		}
-		
-		return sb.toString();
 	}
 }
